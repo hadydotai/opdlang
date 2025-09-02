05ca37689911a02359ac8ea5f13a477c7a1a5ae7
Hady <204673822+hadydotai@users.noreply.github.com>
2025-09-02 17:47:12 +0200

    Handle the run flag on the compile subcommand

--- compile.go
+++ compile.go
@@ -19,28 +19,11 @@ var compileCommand CompileCommand
 
 func (cmd *CompileCommand) Execute(args []string) error {
 	log(LogLevelInfo, "Compiling single file", "file-input", cmd.Args.Files[0], "file-output", cmd.Output)
-	bytes, err := cmd.compileFile()
-	if err != nil {
-		return err
-	}
-
-	log(LogLevelDebug, "Committing output to disk")
-	err = os.WriteFile(cmd.Output, bytes, 0644)
-	if err != nil {
-		return fmt.Errorf("failed to write compiled bytecode to disk: %w", err)
-	}
-
-	log(LogLevelInfo, "Successfully Compiled", "file-input", cmd.Args.Files[0], "file-output", cmd.Output)
-	return nil
-}
-
-func (cmd *CompileCommand) compileFile() ([]byte, error) {
-	log(LogLevelDebug, "Reading file")
 	//TODO(@hadydotai): supporting only one input file for now
 	sourceFile := cmd.Args.Files[0]
 	source, err := os.ReadFile(sourceFile)
 	if err != nil {
-		return nil, fmt.Errorf("failed to read source file %s: %w", sourceFile, err)
+		return fmt.Errorf("failed to read source file %s: %w", sourceFile, err)
 	}
 
 	parser := participle.MustBuild[Program](
@@ -50,17 +33,29 @@ func (cmd *CompileCommand) compileFile() ([]byte, error) {
 	log(LogLevelDebug, "Parsing source")
 	program, err := parser.ParseString(sourceFile, string(source))
 	if err != nil {
-		return nil, fmt.Errorf("parse error: %v", err)
+		return fmt.Errorf("parse error: %v", err)
 	}
 
 	log(LogLevelDebug, "Compilation started")
 	compiler := NewCompiler()
 	bytecode, err := compiler.compileProgram(program)
 	if err != nil {
-		return nil, fmt.Errorf("failed to compile source file %s: %w", sourceFile, err)
+		return fmt.Errorf("failed to compile source file %s: %w", sourceFile, err)
 	}
 
-	return bytecode, nil
+	log(LogLevelDebug, "Committing output to disk")
+	err = os.WriteFile(cmd.Output, bytecode, 0644)
+	if err != nil {
+		return fmt.Errorf("failed to write compiled bytecode to disk: %w", err)
+	}
+
+	log(LogLevelInfo, "Successfully compiled", "file-input", cmd.Args.Files[0], "file-output", cmd.Output)
+
+	if cmd.Run {
+		log(LogLevelInfo, "Running compiled output")
+		runBytecode(compiler)
+	}
+	return nil
 }
 
 func init() {
